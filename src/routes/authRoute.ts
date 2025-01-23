9fc36d4a221d90193997ec6f54738fb3dee5fcf7
HilarioNengareJr <hnengare@gmail.com>
2025-01-23 16:57:08 +0300

    refactor: Rename router and update route paths in authRoute.ts

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -16,14 +16,14 @@ import { Router } from 'express';
  * custom modules
  */
 import { AuthController } from '../controllers/authUser';
-import { isAuthenticated } from '../middlewares/auth_user.middleware';
+import { isAuthenticated } from '../middlewares/authUserMiddleware';
 
-const router = Router();
+const authRouter = Router();
 const authController = new AuthController();
 
-router.get('/admin/login', authController.showLogin);
-router.post('/admin/login', authController.login);
-router.get('/auth/logout', authController.logout);
-router.get('/admin/dashboard', isAuthenticated, authController.showDashboard);
+authRouter.get('/pages/login', authController.showLogin);
+authRouter.post('/pages/login', authController.login);
+authRouter.get('/auth/logout', authController.logout);
+authRouter.get('/pages/dashboard', isAuthenticated, authController.showDashboard);
 
-export default router;
+export default authRouter;
